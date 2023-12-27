b019828b12e246087bf46e2a79efa481e37e3b8d
yohei_hori <yohei_hori@forcia.com>
2023-12-27 14:21:57 +0900

    update

--- gekijo/app.py
+++ gekijo/app.py
@@ -40,161 +40,181 @@ def koyaku():
         result = "ハズレ"
     return result
 
-x = 1
+
+if 'seikou' not in st.session_state.keys():
+    st.session_state['seikou'] = 0
+def seikou():
+    st.session_state['seikou'] = 1
+
+if 'kaisu' not in st.session_state.keys():
+    st.session_state['kaisu'] = 0
+def kaisu():
+    st.session_state['kaisu'] += 1
+
+if 'koyaku' not in st.session_state.keys():
+    st.session_state['koyaku'] = ""
+def get_koyaku():
+    st.session_state['koyaku'] = koyaku()
+
+def game1_3():
+    kaisu()
+
+    
+
+def reset():
+    for key in st.session_state.keys():
+        st.session_state[key] = 0
+
 st.title("激情ジャッジシュミレーター")
 st.header("第1ゲーム")
 
-seikou = 0
-if st.button("レバーを叩け!"):
-    if x == 1:
+
+if st.button("レバーを叩け!", on_click=kaisu):
+    if st.session_state['kaisu'] == 1:
         result = koyaku()
         st.text(result)
         if result == "チャンス目A":
-            seikou = seikou + 1
+            seikou()
         elif result == "チャンス目B":
-            seikou = seikou + 1
+            seikou()
         elif result == "強チェリー":
-            seikou = seikou + 1
+            seikou()
         elif result == "弱チェリー":
             judge = random.randrange(100)
             if judge < 50:
-                seikou = seikou +1
+                seikou()
         elif result == "スイカ":
             judge = random.randrange(100)
             if judge < 50:
-                seikou = seikou +1
+                seikou()
         elif result == "チャンスベル":
             judge = random.randrange(100)
             if judge < 50:
-                seikou = seikou +1
+                seikou()
         elif result == "リプレイ":
             judge = random.randrange(100)
             if judge < 40:
-                seikou = seikou +1
+                seikou()
         elif result == "三枚ベル":
             judge = random.randrange(100)
             if judge < 40:
-                seikou = seikou +1
+                seikou()
         else:
             judge = random.randrange(100)
             if judge == 0:
-                seikou = seikou +1
+                seikou()
     else: 
         st.text("error")
-x = x + 1
+
 st.header("第2ゲーム")
-if st.button("レバーを叩け!!"):
-    if x == 2:
+if st.button("レバーを叩け!!", on_click=kaisu):
+    if st.session_state['kaisu'] == 2:
         result = koyaku()
         st.text(result)
         if result == "チャンス目A":
-            seikou = seikou + 1
+            seikou()
         elif result == "チャンス目B":
-            seikou = seikou + 1
+            seikou()
         elif result == "強チェリー":
-            seikou = seikou + 1
+            seikou()
         elif result == "弱チェリー":
             judge = random.randrange(100)
             if judge < 50:
-                seikou = seikou +1
+                seikou()
         elif result == "スイカ":
             judge = random.randrange(100)
             if judge < 50:
-                seikou = seikou +1
+                seikou()
         elif result == "チャンスベル":
             judge = random.randrange(100)
             if judge < 50:
-                seikou = seikou +1
+                seikou()
         elif result == "リプレイ":
             judge = random.randrange(100)
             if judge < 40:
-                seikou = seikou +1
+                seikou()
         elif result == "三枚ベル":
             judge = random.randrange(100)
             if judge < 40:
-                seikou = seikou +1
+                seikou()
         else:
             judge = random.randrange(100)
             if judge == 0:
-                seikou = seikou +1
+                seikou()
     else: 
         st.text("error")
 
-x = x + 1
 st.header("第3ゲーム")
-if st.button("レバーを叩け!!!"):
-    if x == 3:
-        x = x + 1
+if st.button("レバーを叩け!!!", on_click=kaisu):
+    if st.session_state['kaisu'] == 3:
         result = koyaku()
         st.text(result)
         if result == "チャンス目A":
-            seikou = seikou + 1
+            seikou()
         elif result == "チャンス目B":
-            seikou = seikou + 1
+            seikou()
         elif result == "強チェリー":
-            seikou = seikou + 1
+            seikou()
         elif result == "弱チェリー":
             judge = random.randrange(100)
             if judge < 50:
-                seikou = seikou +1
+                seikou()
         elif result == "スイカ":
             judge = random.randrange(100)
             if judge < 50:
-                seikou = seikou +1
+                seikou()
         elif result == "チャンスベル":
             judge = random.randrange(100)
             if judge < 50:
-                seikou = seikou +1
+                seikou()
         elif result == "リプレイ":
             judge = random.randrange(100)
             if judge < 40:
-                seikou = seikou +1
+                seikou()
         elif result == "三枚ベル":
             judge = random.randrange(100)
             if judge < 40:
-                seikou = seikou +1
+                seikou()
         else:
             judge = random.randrange(100)
             if judge == 0:
-                seikou = seikou +1
+                seikou()
     else: 
         st.text("error")
 
-x = x + 1
+
 st.header("最終ゲーム")
-if st.button("運命を操れ!"):
-    if x == 4:
+if st.button("運命を操れ!", on_click=kaisu):
+    if st.session_state['kaisu'] == 4:
         result = koyaku()
         st.text(result)
         if result == "チャンス目A":
-            seikou = seikou + 1
+            seikou()
         elif result == "チャンス目B":
-            seikou = seikou + 1
+            seikou()
         elif result == "強チェリー":
-            seikou = seikou + 1
+            seikou()
         elif result == "弱チェリー":
-            seikou = seikou + 1
+            seikou()
         elif result == "スイカ":
-            seikou = seikou + 1
+            seikou()
         elif result == "チャンスベル":
-            seikou = seikou + 1
+            seikou()
         elif result == "リプレイ":
-            seikou = seikou + 1
+            seikou()
         elif result == "三枚ベル":
-            seikou = seikou + 1
+            seikou()
         else:
             judge = random.randrange(100)
             if judge < 10:
-                seikou = seikou +1
+                seikou()
     else:
         st.text("Error")
 
-    if seikou != 0:
+    if st.session_state['seikou'] == 1:
         st.text("幕引!!!")
     else:
         st.text("残念")
 
-
-if st.button("リセット"):
-    x = 1
-    seikou = 0
+if st.button("リセット", on_click = reset):
+        st.text("再挑戦！もう一度上から押してね！")
